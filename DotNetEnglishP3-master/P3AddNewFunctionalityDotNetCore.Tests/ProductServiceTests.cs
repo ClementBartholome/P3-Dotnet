d7e136885f6264128d690e1867a2fd57b85072ea
Clément Bartholomé <clementoss@gmail.com>
2024-01-29 13:08:28 +0100

    test(product): tests unitaires pour CheckProductModelErrors et  GetAllProducts

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -1,26 +1,92 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using Xunit;
+using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
+using P3AddNewFunctionalityDotNetCore.Models.Services;
+using Microsoft.Extensions.Localization;
+using Moq;
+using P3AddNewFunctionalityDotNetCore.Models.Entities;
+using P3AddNewFunctionalityDotNetCore.Models.Repositories;
 
 namespace P3AddNewFunctionalityDotNetCore.Tests
 {
     public class ProductServiceTests
     {
-        /// <summary>
-        /// Take this test method as a template to write your test method.
-        /// A test method must check if a definite method does its job:
-        /// returns an expected value from a particular set of parameters
-        /// </summary>
-        [Fact]
-        public void ExampleMethod()
+        private readonly Mock<IStringLocalizer<ProductService>> _localizerMock;
+        private readonly ProductService _productService;
+        private readonly Mock<IProductRepository> _mockProductRepository;
+
+        public ProductServiceTests()
         {
-            // Arrange
-
-            // Act
-
-
-            // Assert
-            Assert.Equal(1, 1);
+            _localizerMock = new Mock<IStringLocalizer<ProductService>>();
+            _mockProductRepository = new Mock<IProductRepository>();
+            _productService = new ProductService(null, _mockProductRepository.Object, null, _localizerMock.Object);
         }
 
-        // TODO write test methods to ensure a correct coverage of all possibilities
+        [Fact]
+        public void CheckProductModelErrors_ReturnsMissingNameError_WhenNameIsNull()
+        {
+            // Arrange
+            var product = new ProductViewModel { Name = null, Stock = 10, Price = 1.99 };
+            var expectedError = "MissingName";
+            var expectedErrors = new List<string> { expectedError };
+            _localizerMock.Setup(_ => _[expectedError]).Returns(new LocalizedString(expectedError, expectedError));
+
+            // Act
+            var result = _productService.CheckProductModelErrors(product);
+
+            // Assert
+            Assert.Equal(expectedErrors, result);
+        }
+
+        [Fact]
+        public void CheckProductModelErrors_ReturnsStockNotGreaterThanZeroError_WhenStockIsZero()
+        {
+            // Arrange
+            var product = new ProductViewModel { Name = "Test", Stock = 0, Price = 1.99 };
+            var expectedError = "StockNotGreaterThanZero";
+            var expectedErrors = new List<string> { expectedError };
+            _localizerMock.Setup(_ => _[expectedError]).Returns(new LocalizedString(expectedError, expectedError));
+
+            // Act
+            var result = _productService.CheckProductModelErrors(product);
+
+            // Assert
+            Assert.Equal(expectedErrors, result);
+        }
+
+        [Fact]
+        public void CheckProductModelErrors_ReturnsPriceNotGreaterThanZeroError_WhenPriceIsZero()
+        {
+            // Arrange
+            var product = new ProductViewModel { Name = "Test", Stock = 10, Price = 0 };
+            var expectedError = "PriceNotGreaterThanZero";
+            var expectedErrors = new List<string> { expectedError };
+            _localizerMock.Setup(_ => _[expectedError]).Returns(new LocalizedString(expectedError, expectedError));
+
+            // Act
+            var result = _productService.CheckProductModelErrors(product);
+
+            // Assert
+            Assert.Equal(expectedErrors, result);
+        }
+        
+        [Fact]
+        public void GetAllProducts_ReturnsAllProducts()
+        {
+            // Arrange
+            var products = new List<Product>
+            {
+                new Product { Name = "Test1", Quantity = 10, Price = 1.99 },
+                new Product { Name = "Test2", Quantity = 10, Price = 1.99 },
+                new Product { Name = "Test3", Quantity = 10, Price = 1.99 }
+            };
+            _mockProductRepository.Setup(repo => repo.GetAllProducts()).Returns(products);
+
+            // Act
+            var result = _productService.GetAllProductsViewModel();
+
+            // Assert
+            Assert.Equal(products.Count, result.Count);
+        }
     }
 }
